2cf2ce686df65b8f890b50a4b45f87f72105a1a2
sandeep0009 <500sandeepks@gmail.com>
2024-11-16 22:20:33 +0530

    done with the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { ShoppingCart } from 'lucide'
+import { FaShoppingCart } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='max-w-4xl m-auto mt-4'>
+        <div className='flex justify-between items-center'>
+            <div className='flex gap-9'>
+                <h2 className='text-2xl font-bold'>sneakers</h2>
+
+                <div className='mt-2'>
+                    <ul className='flex gap-10 text-sm  text-gray-500 '>
+                        <li>Collection</li>
+                        <li>Men</li>
+                        <li>Women</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+
+                <div className=''>
+                    <FaShoppingCart color=''/>
+                    <div className='rounded-full'>
+                        
+
+                    </div>
+
+                   
+                </div>
+            </div>
+            
+        </div>
+        <hr className='mt-6' />
+    </div>
   )
 }
 
