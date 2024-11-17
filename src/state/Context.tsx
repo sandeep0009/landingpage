e26cfcb8ca5ce48deb004fb3621f0e827ad64f44
sandeep0009 <500sandeepks@gmail.com>
2024-11-17 12:56:12 +0530

    conpleted

--- src/state/Context.tsx
+++ src/state/Context.tsx
@@ -1,5 +1,5 @@
 
-import React, { createContext, useState, useContext, ReactNode } from 'react';
+import { createContext, useState, useContext, ReactNode } from 'react';
 
 interface CartContextType {
     count: number;
